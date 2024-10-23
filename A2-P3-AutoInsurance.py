8634f70b9fa1795843dd24ee2ba0b4e88815a097
Matew4202 <w0461513@campus.nscc.ca>
2024-10-23 13:53:20 -0300

    Program 3 done

--- A2-P3-AutoInsurance.py
+++ A2-P3-AutoInsurance.py
@@ -6,13 +6,18 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    # Title
     print("Auto Insurance")
     print("")
+
+    # Ask user to  enter there sex, age, and price of vehicle
 
     sex = input("Are you 'Male' or 'Female': ").lower()
     age = int(input("Enter your age: "))
     price = float(input("Enter the purchase price of the vehicle: $"))
 
+    # Perform calculationd
+    # These shows the rates requires for each sex and age groups
     if sex == "male" and (age >= 15 and age < 25):
         rate = 0.25 / 12
     elif sex == "male" and (age >= 25 and age < 40):
@@ -24,7 +29,7 @@ def main():
     elif sex == "female" and (age >= 25 and age < 40):
         rate = 0.15 / 12
 
-    insurance = price * (rate)
+    insurance = price * (rate) # This calculates the insurance taking the price of the vehicle the mutiply by the required rte
 
     print(f"Your monthly insurance will be ${insurance:.2f}")
     
